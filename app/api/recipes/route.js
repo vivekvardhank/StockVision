184a351066af6608a034902ad52f659ab454bd00
vivek vardhan kalyanapu <kvivek3333@gmail.com>
2024-08-04 07:33:26 -0500

    Update route.js

--- app/api/recipes/route.js
+++ app/api/recipes/route.js
@@ -5,7 +5,7 @@ const OpenAI = require("openai");
 const openai = new OpenAI({ apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY});
 
 export async function POST(req) {
-  const { recipeList } = {"tomato":2,"onion":3,"milk":4};
+  const recipeList  = await req.json();
   try {
     const prompt= "I will give you a JSON object representing pantry items and their quantities. Based on this, create a single recipe. the recipe should be in JSON format and include the recipe name, items with their quantities, and a detailed description of how to make it. I need all the response in single parsed json object."
     const combinedPrompt= `${prompt}\n\nPantry items:\n${JSON.stringify(recipeList, null, 2)}`;
@@ -14,7 +14,7 @@ export async function POST(req) {
         role: "system",
         content:[ {
             type:"text",
-            text:"return the json structure based on user requirement. Only return the json structure, nothing else. in the sample format of {\"recipe_name\":\"Recipe Name\",\"items\":{\"item1\":\"quantity\",\"item2\":\"quantity\",...},\"description\":\"Step-by-step instructions on how to make the recipe.\" ```json}",     
+            text:"return the json structure based on user requirement. Only return the json structure, nothing else. in the sample format of {\"recipe_name\":\"Recipe Name\",\"items\":{\"item1\":\"quantity\",\"item2\":\"quantity\",...},\"description\":\"how to make the recipe in steps(just steps).\" ```json}",     
         },
     ],
     },
@@ -32,6 +32,7 @@ export async function POST(req) {
     });
 
     const recipe = response.choices[0].message.content.trim();
+    
 
     if (recipe) {
         return new Response(JSON.stringify({ recipe }), { status: 200 });
